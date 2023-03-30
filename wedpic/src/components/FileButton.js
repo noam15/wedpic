e4cdb87ef0e6c45870a35e951dae4581f55f5c9a
Hillel Nagid <hillel123123@gmail.com>
2023-03-30 16:12:23 +0300

    Fixed sending pics through state

--- wedpic/src/components/FileButton.js
+++ wedpic/src/components/FileButton.js
@@ -3,47 +3,29 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const FileButton = () => {
-  const MAX_COUNT = 10;
-  const [uploadedFiles, setUploadedFiles] = useState([]);
-  const [fileLimit, setFileLimit] = useState(false);
-  let navigate = useNavigate();
-  const handleUploadFiles = (files) => {
-    const uploaded = [...uploadedFiles];
-    let limitExceeded = false;
-    files.some((file) => {
-      if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-        uploaded.push(file);
-        if (uploaded.length === MAX_COUNT) setFileLimit(true);
-        if (uploaded.length > MAX_COUNT) {
-          alert(`You can only add a maximum of ${MAX_COUNT} files`);
-          setFileLimit(false);
-          limitExceeded = true;
-          return true;
-        }
-      }
-    });
-    if (!limitExceeded) setUploadedFiles(uploaded);
-  };
+	let navigate = useNavigate();
+	const handleFileEvent = (e) => {
+		const chosenFiles = Array.prototype.slice.call(e.target.files);
+		if (chosenFiles.length > 10 || !chosenFiles.length)
+			alert(`You can only add a maximum of 10 files`);
+		else {
+			navigate('/pics', { state: { chosenFiles } });
+		}
+	};
 
-  const handleFileEvent = (e) => {
-    const chosenFiles = Array.prototype.slice.call(e.target.files);
-    handleUploadFiles(chosenFiles);
-    navigate('/pics', { state: uploadedFiles });
-  };
-
-  return (
-    <>
-      <button className='filebutton'>
-        <input
-          type='file'
-          multiple
-          accept='image/png'
-          className='transperentbutton'
-          onChange={handleFileEvent}
-          disabled={fileLimit}
-        />
-      </button>
-    </>
-  );
+	return (
+		<>
+			<button className='filebutton'>
+				<input
+					type='file'
+					multiple
+					accept='image/png'
+					className='transperentbutton'
+					onChange={handleFileEvent}
+					disabled={fileLimit}
+				/>
+			</button>
+		</>
+	);
 };
 export default FileButton;
--- wedpic/src/components/ImageComponent.js
+++ wedpic/src/components/ImageComponent.js
@@ -1,12 +1,11 @@
-import "./ImageComponent.css"
-//import logo from '../trypicfierwork.jpg';
+import './ImageComponent.css';
+import fireworks from '../trypicfierwork.jpg';
 
-const ImageComponent=()=>{
-    return (        
-    <>
-    <img src="../trypicfierwork.jpg" height={200} width={200}></img>
-    </>
-          
-      )
-}
-export default ImageComponent
+const ImageComponent = () => {
+	return (
+		<>
+			<img src={fireworks} height={200} width={200}></img>
+		</>
+	);
+};
+export default ImageComponent;
