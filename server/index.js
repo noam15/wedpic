e17c72455fe8d0fad676bf64a76e2722b83b27f4
Hillel Nagid <hillel123123@gmail.com>
2023-04-18 18:38:05 +0300

    Photos auto order by timeframe

--- server/index.js
+++ server/index.js
@@ -23,112 +23,122 @@ app.use(fileUpload());
 app.use(express.static('uploads'));
 
 const uri =
-	'mongodb+srv://hillel:325605384@wedpic.4f6etky.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://hillel:325605384@wedpic.4f6etky.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 client.connect((err) => {
-	console.error(err);
-	client.close();
+  console.error(err);
+  client.close();
 });
 
 const timeFramesCollection = client.db('wedpic').collection('timeframes');
 
 app.get('/getTimeFrames', async (req, res) => {
-	try {
-		const timeFrames = await timeFramesCollection.find({}).toArray();
-		res.send(timeFrames);
-	} catch (err) {
-		console.error(err);
-		req.send(err);
-	}
+  try {
+    const timeFrames = await timeFramesCollection.find({}).toArray();
+    res.send(timeFrames);
+  } catch (err) {
+    console.error(err);
+    req.send(err);
+  }
 });
 
 app.get('/getImages', (req, res) => {
-	try {
-		const pics = [];
-		fs.readdir(join(__dirname, '/../uploads'), (err, files) => {
-			files.forEach((file) => {
-				pics.push(
-					fs
-						.readFileSync(join(__dirname, '/../uploads', file))
-						.toString('base64')
-				);
-			});
-			res.send(pics);
-		});
-	} catch (error) {
-		res.send(error);
-	}
+  try {
+    const pics = [];
+    fs.readdir(join(__dirname, '/../uploads'), (err, files) => {
+      files.forEach((file) => {
+        pics.push(
+          fs
+            .readFileSync(join(__dirname, '/../uploads', file))
+            .toString('base64')
+        );
+      });
+      res.send(pics);
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.post('/addImages', (req, res) => {
-	try {
-		console.log(req.files);
-		req.files.files.forEach(async (img) => {
-			const fileType = await fileTypeFromBuffer(img.data);
-			let subfolder = '';
-			new ExifImage({ image: img.data }, (err, metadata) => {
-				if (err) console.error(err);
-				else {
-					timeFramesCollection
-						.find({})
-						.toArray()
-						.forEach((timeFrame) => {
-							const dateAndTime = metadata.exif.DateTimeOriginal.split(' ');
-							const date = dateAndTime[0].replace(':', '/');
-							const time = dateAndTime[1];
-							const imgTakenAt = new Date(date + ' ' + time);
-							const timeFrameStarts = new Date(timeFrame.startTime);
-							const timeFrameEnds = new Date(timeFrame.endTime);
-							if (imgTakenAt >= timeFrameStarts && imgTakenAt < timeFrameEnds) {
-								subfolder = timeFrame.title;
-							}
-						});
-				}
-				if (subfolder) {
-					const uuid = randomUUID();
-					if (fs.existsSync(join(__dirname, '/../uploads', subfolder))) {
-					} else {
-						fs.mkdirSync(join(__dirname, '/../uploads', subfolder));
-					}
-					img.mv(
-						join(
-							__dirname,
-							'/../uploads',
-							subfolder,
-							uuid + '.' + fileType.ext
-						),
-						(e) => {
-							if (e) console.error(e);
-						}
-					);
-				}
-			});
-		});
-		res.send('👍');
-	} catch (error) {
-		res.status(400).send(error);
-	}
+  try {
+    const saveImg = async (img) => {
+      const fileType = await fileTypeFromBuffer(img.data);
+      let subfolder = '';
+      new ExifImage({ image: img.data }, async (err, metadata) => {
+        if (err) console.error(err);
+        else {
+          try {
+            await timeFramesCollection.find({}).forEach((timeFrame) => {
+              const dateAndTime = metadata.exif.DateTimeOriginal.split(' ');
+              const date = dateAndTime[0].replace(':', '/').replace(':', '/');
+              const time = dateAndTime[1];
+              const timeFrameStarts = new Date(parseInt(timeFrame.startTime));
+              const timeFrameEnds = new Date(parseInt(timeFrame.endTime));
+              const imgTakenAt = new Date(date + ' ' + time);
+              imgTakenAt.setFullYear(timeFrameStarts.getFullYear());
+              imgTakenAt.setMonth(timeFrameStarts.getMonth());
+              imgTakenAt.setDate(timeFrameStarts.getDate());
+              if (imgTakenAt >= timeFrameStarts && imgTakenAt < timeFrameEnds) {
+                subfolder = timeFrame.name;
+              }
+            });
+          } catch (err) {
+            console.error(err);
+          }
+        }
+
+        if (subfolder) {
+          const uuid = randomUUID();
+          if (!fs.existsSync(join(__dirname, '/../uploads', subfolder))) {
+            fs.mkdir(join(__dirname, '/../uploads', subfolder), (err, path) => {
+              if(err)console.error(err);
+            });
+          }
+          img.mv(
+            join(
+              __dirname,
+              '/../uploads',
+              subfolder,
+              uuid + '.' + fileType.ext
+            ),
+            (e) => {
+              if (e) console.error(e);
+            }
+          );
+        }
+      });
+    };
+    if (Array.isArray(req.files.files)) {
+      req.files.files.forEach(saveImg);
+    } else {
+      saveImg(req.files.files);
+    }
+    res.send('👍');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.put('/editTimeFrame', (req, res) => {
-	const { _id, change } = req.body;
-	try {
-		const result = timeFramesCollection.updateOne(
-			{ _id: new ObjectId(_id) },
-			{ $set: change }
-		);
-		res.send(result);
-	} catch (error) {
-		res.send(req.body);
-	}
+  const { _id, change } = req.body;
+  try {
+    const result = timeFramesCollection.updateOne(
+      { _id: new ObjectId(_id) },
+      { $set: change }
+    );
+    res.send(result);
+  } catch (error) {
+    res.send(req.body);
+  }
 });
 
 app.listen(8080, () => {
-	console.log(`We're up!`);
+  console.log(`We're up!`);
 });
