b5457d4da675b805ca4f7de2f06a4f9d2141d796
Hillel Nagid <hillel123123@gmail.com>
2023-04-18 15:34:51 +0300

    trying local uploads

--- wedpic/src/components/FileButton.js
+++ wedpic/src/components/FileButton.js
@@ -3,28 +3,28 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const FileButton = () => {
-  let navigate = useNavigate();
-  const handleFileEvent = (e) => {
-    const chosenFiles = Array.prototype.slice.call(e.target.files);
-    if (chosenFiles.length > 10 || !chosenFiles.length)
-      alert(`You can only add a maximum of 10 files`);
-    else {
-      navigate('/pics', { state: { chosenFiles } });
-    }
-  };
+	let navigate = useNavigate();
+	const handleFileEvent = (e) => {
+		const chosenFiles = Array.prototype.slice.call(e.target.files);
+		if (chosenFiles.length > 10 || !chosenFiles.length)
+			alert(`You can only add a maximum of 10 files`);
+		else {
+			navigate('/pics', { state: { chosenFiles } });
+		}
+	};
 
-  return (
-    <>
-      <button className='filebutton'>
-        <input
-          type='file'
-          multiple
-          accept='image/png'
-          className='transperentbutton'
-          onChange={handleFileEvent}
-        />
-      </button>
-    </>
-  );
+	return (
+		<>
+			<button className='filebutton'>
+				<input
+					type='file'
+					multiple
+					accept='image/*'
+					className='transperentbutton'
+					onChange={handleFileEvent}
+				/>
+			</button>
+		</>
+	);
 };
 export default FileButton;
