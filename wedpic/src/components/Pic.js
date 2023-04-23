04a9e0fd3e2ccd6e0105ca2e08e53a7ac7b5a64b
Hillel Nagid <hillel123123@gmail.com>
2023-04-23 23:03:46 +0300

    Improving the desgin sorta kinda

--- wedpic/src/components/Pic.js
+++ wedpic/src/components/Pic.js
@@ -1,13 +1,6 @@
-const Pic = ({ src, onClick }) => {
-	const arrayBufferToBase64 = (buffer) => {
-		let binary = '';
-		let bytes = new Uint8Array(buffer);
-		let len = bytes.byteLength;
-		for (let i = 0; i < len; i++) {
-			binary += String.fromCharCode(bytes[i]);
-		}
-		return window.btoa(binary);
-	};
+import { FaTrash } from 'react-icons/fa';
+
+const Pic = ({ src, onClick, onClose, deleteFile, isModal }) => {
 	return (
 		<div
 			className='div-container'
@@ -20,13 +13,60 @@ const Pic = ({ src, onClick }) => {
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
+				position: 'relative',
+				border: 'solid 1em white',
 			}}
+			onClick={onClick}
 		>
+			{isModal && (
+				<div
+					className='buttons'
+					style={{
+						position: 'absolute',
+						top: '1.2em',
+						fontSize: '.8em',
+						zIndex: '13',
+						padding: 0,
+						width: '100%',
+					}}
+				>
+					<p
+						onClick={onClose}
+						style={{
+							position: 'absolute',
+							top: 0,
+							margin: 0,
+							right: '1em',
+							zIndex: '15',
+						}}
+					>
+						X
+					</p>
+					<p
+						onClick={deleteFile}
+						style={{
+							position: 'absolute',
+							top: 0,
+							margin: 0,
+							left: '1em',
+							zIndex: '15',
+						}}
+					>
+						<FaTrash />
+					</p>
+				</div>
+			)}
 			<img
-				style={{ width: '35vw', padding: '.2em' }}
-				src={URL.createObjectURL(src)}
+				style={{
+					width: '35vw',
+					minWidth: '35vw',
+					padding: '.2em',
+					height: '35vw',
+					objectFit: 'cover',
+					borderRadius: '1em',
+				}}
+				src={src}
 				alt='img'
-				onClick={onClick}
 			/>
 		</div>
 	);
