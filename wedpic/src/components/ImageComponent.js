f10d7896b766bfdfedad32e04f80809b549b0626
Hillel Nagid <hillel123123@gmail.com>
2023-05-07 10:20:49 +0300

    Change server url

--- wedpic/src/components/ImageComponent.js
+++ wedpic/src/components/ImageComponent.js
@@ -1,14 +1,17 @@
 import './ImageComponent.css';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import im1 from '../assets/try1.jpg';
+import im3 from '../assets/try2.jpg';
+import im2 from '../assets/try3.jpg';
 
 const ImageComponent = () => {
-	const [pics, setPics] = useState([]);
-	useEffect(() => {
-		fetch('http://localhost:8080/getImages')
-			.then((res) => res.json())
-			.then((res) => setPics(res));
-	}, []);
+	const [pics, setPics] = useState([im1, im2, im3]);
+	// useEffect(() => {
+	//   fetch("http://localhost:8080/getImages")
+	//     .then((res) => res.json())
+	//     .then((res) => setPics(res));
+	// }, []);
 	return (
 		<>
 			{pics &&
