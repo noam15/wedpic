ac57b55cc44079826c24d54c96ff1ad2a4a09a34
noam <nom21195@gmail.com>
2023-05-14 08:33:43 +0300

    change background

--- wedpic/src/HomePage/HomePage.js
+++ wedpic/src/HomePage/HomePage.js
@@ -5,20 +5,16 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div className="HomePage">
-      <div className="imagediv">
+      
         <div className="squarediv">
           <div className="App-header">
             <a className="headlinestyle"> בר & ישי</a>
             <p>17.5.2023</p>
             <div className="explainingviv">
-              {" "}
-              אז על מה לעזזל אתם מסתכלים? באתר זה נמצא האלבום הדיגיטלי של החתונה
-              אז איך זה עובד? בעזרתכם כמובן, בכפתור מטה תוכלו להעלות עד 10
-              תמונות שצילמתם באירוע ואם תשיכו ללגלול תוכלו לצפות באלבום שמתחיל
-              להיווצר מהתמונות שעולות
+           
             </div>
           </div>
-        </div>
+     
       </div>
 
       <div className="Grid-page">
