e7f1b3e8c63b5f97165e420596cf49dead9a7dc4
Hillel Nagid <hillel123123@gmail.com>
2023-05-14 18:58:30 +0300

    just styling left

---
+++ wedpic/src/ThankYou/ThankYou.js
@@ -0,0 +1,5 @@
+const ThankYou = () => {
+	return <p>תודה על העלאת התמונות, אתם יכולים לסגור את הדף</p>;
+};
+
+export default ThankYou;
--- wedpic/src/components/FileButton.js
+++ wedpic/src/components/FileButton.js
@@ -1,12 +1,14 @@
-import "./FileButton.css";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import './FileButton.css';
+import { useNavigate } from 'react-router-dom';
 
-const FileButton = () => {
+const FileButton = ({ numOfUploadedPics }) => {
 	let navigate = useNavigate();
 	const handleFileEvent = (e) => {
 		const chosenFiles = Array.prototype.slice.call(e.target.files);
-		if (chosenFiles.length > 10 || !chosenFiles.length)
+		if (
+			(numOfUploadedPics ?? 0) + chosenFiles.length > 10 ||
+			!chosenFiles.length
+		)
 			alert(`You can only add a maximum of 10 files`);
 		else {
 			navigate('/pics', { state: { chosenFiles } });
@@ -15,17 +17,20 @@ const FileButton = () => {
 
 	return (
 		<>
-			<button className='filebutton'>
+			<button
+				className='filebutton'
+				disabled={numOfUploadedPics >= 10 ? true : false}
+			>
+				<p className='btntxt'>העלאת תמונות</p>
 				<input
 					type='file'
 					multiple
-					accept='image/*'
+					accept='image/jpeg'
 					className='transperentbutton'
 					onChange={handleFileEvent}
 				/>
 			</button>
 		</>
 	);
-
 };
 export default FileButton;
