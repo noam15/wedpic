742599ffd812a74cdf4fa0324eae486a14505776
noam <nom21195@gmail.com>
2023-05-15 22:41:29 +0300

    web icon

--- wedpic/src/ThankYou/ThankYou.js
+++ wedpic/src/ThankYou/ThankYou.js
@@ -1,10 +1,10 @@
 import "./ThankYou.css"
-
+import mimions from '../assets/minion-love-kevin-love.gif'
 
 const ThankYou = () => {
 	return <div className="placer">
 		<p className="text1">תודה על העלאת התמונות</p>
-		<div  className="imageminons"  />
+		<img  className="imageminons" src={mimions} />
 		<p className="text2">זהו, אתם יכולים לסגור את הדף </p>
 		
 	</div>
