e970b9f91c33fd7804b1b1ebf7045f5545edd269
Hillel Nagid <hillel123123@gmail.com>
2023-05-16 11:43:30 +0300

    FINISHNOO

--- wedpic/src/ThankYou/ThankYou.js
+++ wedpic/src/ThankYou/ThankYou.js
@@ -2,13 +2,13 @@ import "./ThankYou.css"
 import mimions from '../assets/minion-love-kevin-love.gif'
 
 const ThankYou = () => {
-	return <div className="placer">
-		<p className="text1">תודה על העלאת התמונות</p>
-		<img  className="imageminons" src={mimions} />
-		<p className="text2">זהו, אתם יכולים לסגור את הדף </p>
-		
-	</div>
-	
+  return <div className="placer">
+    <p className="text1">תודה על העלאת התמונות</p>
+    
+    <p className="text2">זהו, אתם יכולים לסגור את הדף </p>
+    
+  </div>
+  
 };
 
 export default ThankYou;
